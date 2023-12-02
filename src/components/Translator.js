c9243297ddb9f05cf71f0430d7abe19f7b1609b1
Donovan Gibson <donovan.gibson89@gmail.com>
2023-12-01 22:40:36 -0600

    Translator.js I'm having trouble with the environmental variable, but Once its settled I should be able to translate text using OpenAPI by selecting a radio button to add the prompt to translate via the selected language.

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,8 +1,47 @@
-import React from "react";
-import OpenAI from "openai";
+import React, { useState } from 'react';
+import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+import OpenAI from 'openai';
 
-function Translator() {
-  const [inputText, setInputText] = useState("");
+const openai = new OpenAI({dangerouslyAllowBrowser:true,
+  apiKey: process.env.OPENAI_API_KEY });
+
+const Translator = () => {
+  const [inputText, setInputText] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState('french');
+  const [translatedText, setTranslatedText] = useState('');
+
+  const handleTranslation = async () => {
+    try {
+      let promptLanguage;
+      switch (selectedLanguage) {
+        case 'french':
+          promptLanguage = "French";
+          break;
+        case 'spanish':
+          promptLanguage = "Spanish";
+          break;
+        case 'korean':
+          promptLanguage = "Korean";
+          break;
+        default:
+          promptLanguage = "French";
+      }
+
+      const completion = await openai.chat.completions.create({
+        messages: [
+          { "role": "system", "content": "You are a helpful assistant." },
+          { "role": "user", "content": `Translate this to ${promptLanguage}: ${inputText}` }
+        ],
+        model: "gpt-3.5-turbo",
+      });
+
+      const translatedText = completion.choices[0].message.content;
+      setTranslatedText(translatedText);
+    } catch (error) {
+      console.error('Error during translation:', error);
+      setTranslatedText('Error during translation.');
+    }
+  };
 
   return (
     <div>
@@ -14,8 +53,28 @@ function Translator() {
           onChange={(e) => setInputText(e.target.value)}
         />
       </InputGroup>
+
+      <Form>
+        {['french', 'spanish', 'korean'].map((lang) => (
+          <Form.Check 
+            key={lang}
+            type="radio"
+            label={lang.charAt(0).toUpperCase() + lang.slice(1)}
+            name="languageOptions"
+            id={`radio-${lang}`}
+            onChange={() => setSelectedLanguage(lang)}
+            checked={selectedLanguage === lang}
+          />
+        ))}
+      </Form>
+
+      <Button onClick={handleTranslation} className="mt-3">Translate</Button>
+
+      <div className="translation-output mt-3">
+        {translatedText}
+      </div>
     </div>
   );
-}
+};
 
 export default Translator;
