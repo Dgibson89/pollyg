962ff7a28b168ae0cd0dde3a0b9fdc498b0103df
Dgibson89 <donovan.gibson89@gmail.com>
2023-12-04 11:44:58 -0600

    App.js Passed the selected language state up to be able to use it in both the Header and Translator components and passed the selected language as props to both.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import React, { useState } from 'react';
 import Header from "./components/Header";
 import Translator from "./components/Translator";
 
 function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState('french');
+
   return (
     <div className="App">
-      <Header />
-      <Translator />
+      <Header selectedLanguage={selectedLanguage} />
+      <Translator selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
     </div>
   );
 }
