c83f6586341d9452d623e7c0e5f93b82b6cd9227
Dgibson89 <donovan.gibson89@gmail.com>
2023-12-04 11:46:22 -0600

    Header.js Added the ability for the background image to change based on the selected language using pictures for unsplash.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,40 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
-const Header = () => {
-  return <Container>
-    <Row>
-        <Col><p>Header</p></Col>
-    </Row>
-  </Container>
+const Header = ({ selectedLanguage }) => {
+  const getBackgroundImage = () => {
+    switch (selectedLanguage) {
+      case "english":
+        return "url(https://images.unsplash.com/photo-1543015330-05fcfca844ad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"; 
+      case "spanish":
+        return "url(https://images.unsplash.com/photo-1503187680590-525b6e7a793f?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"; 
+      case "korean":
+        return "url(https://images.unsplash.com/photo-1544094552-172c2153499e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+      case "minionese":
+        return "url(https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"; 
+      case "french":
+      default:
+        return "url(https://images.unsplash.com/photo-1543349689-9a4d426bee8e?q=80&w=2001&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"; 
+    }
+  };
+
+  const headerStyle = {
+    backgroundImage: getBackgroundImage(),
+    backgroundSize: 'contain',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '500px',
+    width: '100%',
+    borderRadius: '60px',
+    transition: 'background-image 0.5s ease'
+  };
+  
+
+  return (
+    <Container style={headerStyle} className="mb-4">
+      <h1>Polyglot Bot</h1>
+    </Container>
+  );
 };
 
 export default Header;
